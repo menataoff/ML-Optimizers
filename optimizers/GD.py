92b3710b54ee9acdc0c09b38446541ac412f6f8f
menataoff <myk1n.alex@gmail.com>
2026-02-08 19:34:41 +0300

    Updated GD

--- optimizers/GD.py
+++ optimizers/GD.py
@@ -8,11 +8,16 @@ class GradientDescent(BaseOptimizer):
     """
     def __init__(self,
                  parameters: Iterable[Union[np.ndarray, list, tuple, Parameter]],
-                 lr: float = 0.01) -> None:
+                 lr: float = 0.01,
+                 weight_decay: float = 0.0) -> None:
         super().__init__(parameters, lr)
+        self._weight_decay = weight_decay
+
+    def __repr__(self):
+        return f"GradientDescent(lr={self.lr}, params={len(self._params)})"
 
     def _update_parameters(self) -> None:
         for param in self._params:
-            param.data -= self.lr * param.grad
+            param.data -= self.lr * (param.grad + self._weight_decay * param.grad)
 
     #TODO: изменить работу с массивами, так как есть проблема с этим
